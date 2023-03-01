e55ebf95b53485c27d3da7ee1c83c3190251bf8b
Touhidur Zaman <touhidcodes@gmail.com>
2023-03-02 01:49:41 +0600

    object-create-methods

--- object.js
+++ object.js
@@ -1,4 +1,53 @@
-const obj = {
-	name: "touhid",
-	id: 24,
+// create oject using object literals
+const player = {};
+
+// Properties
+player.name = "nirob";
+player.age = 24;
+
+// Method
+player.bat = () => {
+	console.log("swing your bat");
 };
+// console.log(player);
+// player.bat();
+
+const student = {
+	name: "pandey",
+	job: "undey",
+	ball: () => {
+		console.log("throw your ball");
+	},
+	salary: 20000,
+};
+
+// Object Constructor
+const person = new object();
+console.log(person);
+
+// Object create method
+const item = Object.create(null);
+console.log(item);
+
+const stu = Object.create(student);
+console.log(stu.name);
+
+// Class
+class Person {
+	name = "abul";
+	address = "sadarghat";
+	constructor(age) {
+		this.age = age;
+	}
+}
+
+const person1 = new Person(34);
+console.log(person1);
+
+// Function
+function Car(model, year) {
+	this.model = model;
+	this.year = year;
+}
+
+const tesla = new Car("elon", 2026);
